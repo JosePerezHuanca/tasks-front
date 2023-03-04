0151d9c73a753c51351c6fd9769d18d36fe056af
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-03-04 20:55:15 -0300

    Se establece la propiedad disableHostCheck como true

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    disableHostCheck: true
   }
 })
