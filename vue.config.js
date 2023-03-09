9289ecc89d8528ede794882dc21821a380c32f91
Jose Perez Huanca <perezhuancajose@gmail.com>
2023-03-09 15:09:29 -0300

    Se borra la propiedad disableHost check

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-    port: process.env.PORT || 3000,
-    disableHostCheck: true
+    port: process.env.PORT || 3000
   }
 })
